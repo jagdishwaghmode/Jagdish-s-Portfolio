b25235f4ac42bca54f9e3ef1a29be327422719b5
jagdishwaghmode <jagdishwaghmode03@gmail.com>
2025-06-13 11:45:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll('.timeline-item');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+items.forEach(item => observer.observe(item));
